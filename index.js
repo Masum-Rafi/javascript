7d24b9e8bb4a00164fd53f5526b1a2f373047d1f
Masum-Rafi <masumbillahrafi0@gmail.com>
2025-08-27 15:17:03 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+const me = "25";
+const friend = 25;
+console.log("equal operator", me == friend);
+console.log("Identity Operator", me === friend);
+// Here in variable called me hold a string type of data and variable friend holds a intiger type data. in our 1st console we use equal opertor and 2nd console we use identity operator, identity operator checks is they hold similer valus and also checks are thay same data type also where equal operator only checks 

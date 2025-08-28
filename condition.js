775db06998e9219041879849962d2c2bc873dd49
Masum-Rafi <masumbillahrafi0@gmail.com>
2025-08-28 22:28:11 +0600

    Practice some condition  questions

---
+++ condition.js
@@ -0,0 +1,23 @@
+const result = 45;
+if (result >= 40) {
+console.log("New bike");
+} else {
+   console.log("Dhur beta");
+}
+
+const show = "9pm";
+const nextShow = "10pm";
+if (show <= nextShow){
+   console.log("its movie time");
+} else {
+   console.log("only Sleep");
+}
+
+
+const tamparaturelimit = 30;
+const tamparatureNow = 32;
+if (tamparatureNow >= tamparaturelimit) {
+   console.log("Ac On");
+} else {
+   console.log("Blanket on my body");
+}

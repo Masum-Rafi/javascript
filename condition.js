20b7bba5706f792a8cb0dc098a4d93b2d54cf15a
Masum-Rafi <masumbillahrafi0@gmail.com>
2025-08-28 22:57:41 +0600

    Practice some condition questions with AND & OR operators

--- condition.js
+++ condition.js
@@ -21,3 +21,40 @@ if (tamparatureNow >= tamparaturelimit) {
 } else {
    console.log("Blanket on my body");
 }
+
+
+//AND & OR condition
+const ageM = 25;
+const hight = 72;
+if (ageM > 18 && hight > 60) {
+  console.log("push the car bro");
+} else {
+  console.log("YOU ARE A CHILD,SITE ON THE CAR");
+}
+
+const mathScore = 95;
+const englishScore = 70;
+if (mathScore > 80 || englishScore > 85) {
+  console.log("You are qualified for admission ");
+} else {
+  console.log("Go marry someone");
+}
+
+const GPA = 5;
+const monthlyEarning = 12000;
+if(GPA == 5 && monthlyEarning <= 10000 ){
+   console.log("You got the scholership")
+} else {
+   console.log("You are not qualified");
+}
+
+const age = 27;
+const workexp = 3;
+const maxAge = 30;
+const minWorkexp = 2;
+if (maxAge < age && minWorkexp > workexp){
+   console.log("You are qualified for job exm")
+} else {
+   console.log("You are not qualified for this");
+}
+//not solved yet

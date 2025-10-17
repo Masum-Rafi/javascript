b98a9d1be9311323a95a14a89e508d89fcaed768
Masum-Rafi <masumbillahrafi0@gmail.com>
2025-10-17 12:14:00 +0600

    Complete Object practie

---
+++ object.js
@@ -0,0 +1,90 @@
+const { Children } = require("react");
+
+const student = {
+  name: "Maruf",
+  age: 15,
+  class: 10,
+  isSingle: true,
+};
+
+const phone = {
+  Brand: "Apple",
+  Model: "12 PM",
+  Color: "Blue",
+  Price: 80000,
+};
+//there can be some array and also an object inside of object
+
+const subject = {
+  name: "Biology",
+  teacher: "Kuddus",
+  exmDate: "30 dec",
+  chapter: [1, 2, "Third"],
+  nextExm: {
+    name: "Final",
+    marks: 100,
+  },
+};
+
+const teacher = {
+  name: "Xudling - pong",
+  Age: 45,
+  Depertment: "Math",
+  Home: "Gazipur",
+  classes: ["Sunday", "Monday", "Wensday"],
+};
+
+const Tree = {
+  name: "Shimul",
+  hight: "30 ft",
+  age: "25 y",
+  frootName: "Simul",
+};
+
+const human = {
+  name: "Tuhin",
+  age: "32y",
+  ocupassion: "Student & Job",
+  Depertment: ["CSE", "Tec Support"],
+  family: {
+    wife: true,
+    Children: true,
+    ParrentsAlive: true,
+  },
+};
+
+const Pops = {
+  Name: "Kalam Hawla",
+  age: 65,
+  ocupassion: "Retierd",
+  family: {
+    child: 3,
+    boy: 2,
+    Girl: 1,
+  },
+};
+
+const motorbike = {
+  Brand: "Yammaha",
+  Tyre: 2,
+  color: "Red",
+  topSpeed: 110,
+  price: 320000,
+};
+
+const bird = {
+  name: "love bird",
+  color: ["Red", "Black", "white"],
+  food: ["rice", "water", "bajra", "ETC"],
+};
+
+const laptop = {
+  Brand: "HP",
+  processor: "Intel i3 7thgen",
+  ram: ["16gb", "DDR4"],
+  display: {
+    Ratio: 1280 * 720,
+    Type: "LCD",
+  },
+  price: 45000,
+};
